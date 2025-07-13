679c287fec2dc0d7f560457d469f2015527f3e10
Botond Sulyok <botond.sulyok@kh.hu>
2025-07-13 20:57:46 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // IMPORTANT: Change '/curve-war/' to your repository name
   base: '/curve-war/',
   build: {
     outDir: 'dist'
